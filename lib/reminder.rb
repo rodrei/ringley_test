72c2f1c924156730754a7b93cf0cec46dfa7a937
Rodrigo Pavano <rodrigopavano@gmail.com>
2011-11-02 13:09:02 -0300

    Reminder#on refactor

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -11,17 +11,20 @@ class Reminder
     reminders = {}
 
     estates.each do |e|
+      curr_reminders = []
+
       e.due_dates.each do |due_date|
         curr_year_due_date = Date.new(date.year, due_date[:month], due_date[:day])
         next_year_due_date = curr_year_due_date + 1.year
+
         if(date >= curr_year_due_date - @reminder_time[e.charge_period] and date <= curr_year_due_date)
-          reminders[e.code] ||= []
-          reminders[e.code] << curr_year_due_date
+          curr_reminders << curr_year_due_date
         elsif(date >= next_year_due_date - @reminder_time[e.charge_period])
-          reminders[e.code] ||= []
-          reminders[e.code] << next_year_due_date
+          curr_reminders << next_year_due_date
         end
       end
+
+      reminders[e.code] = curr_reminders if curr_reminders.any?
     end
 
     reminders
